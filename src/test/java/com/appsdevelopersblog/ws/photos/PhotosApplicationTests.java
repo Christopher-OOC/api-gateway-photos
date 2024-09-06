ebdfffa5cc724580e8a15809717fd6b6434a3211
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-06 10:12:02 +0100

    commit

---
+++ src/test/java/com/appsdevelopersblog/ws/photos/PhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdevelopersblog.ws.photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
